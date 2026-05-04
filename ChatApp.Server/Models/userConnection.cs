3fe574d096e1b9380c28ccc720d7d79be7098be7
Zeebo <sebastian.gabrielsson03@gmail.com>
2026-05-04 11:47:20 +0200

    initial commit

---
+++ ChatApp.Server/Models/userConnection.cs
@@ -0,0 +1,8 @@
+namespace ChattApp.Server.Models
+{
+    public class UserConnection
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string ChatRoom { get; set; } = string.Empty;
+    }
+}
